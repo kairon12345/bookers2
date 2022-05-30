317441f51c86989f4a39888191ba10e88304056b
EC2 Default User <e3744d7fd77ayzE05SE@ymobile.ne.jp>
2022-05-30 14:08:48 +0000

    totyuu

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ApplicationRecord
+  belongs_to :user
+
+  validates :name,presence: true
+  validates :body,presence: true
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '53d8f6d28074372d73650a0dccd920462000adf0f797337a22df27e8a7a32826f88d476cb0799a1bd2aaaea58c3efa80a39ba4e4e4d8da795f70b0dac23f4f07'
